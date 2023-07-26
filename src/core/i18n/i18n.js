fa25a1fdfeefda211e9a914153073bafe9d00d7e
Max Sidorenko <fivemoshr@gmail.com>
2023-07-26 21:20:09 +0300

    fix(core):change base url

--- src/core/i18n/i18n.js
+++ src/core/i18n/i18n.js
@@ -4,7 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 // to do translaion.json
-const FRONTEND_BASE_URL = 'https://serjkenri.github.io';
+const FRONTEND_BASE_URL = 'https://kenobiwins.github.io';
 
 i18next
     .use(initReactI18next)
